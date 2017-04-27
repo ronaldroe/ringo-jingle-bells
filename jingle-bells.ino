173d26e8f66d3c62b95c9c3072ef45932eafe75f
Ronald Roe <rroedesign@gmail.com>
2017-04-26 21:22:58 -0500

    Update jingle-bells.ino

--- jingle-bells.ino
+++ jingle-bells.ino
@@ -1,25 +1,3 @@
-
-/* 
-
-Ringo Robot
-Ringo_Base_Sketch_Rev06
-Version 6.1 12/2015
-
-This is a basic sketch that can be used as a starting point
-for various functionality of the Ringo robot.
-
-Significant portions of this code written by
-Dustin Soodak for Plum Geek LLC. Some portions
-contributed by Kevin King.
-Portions from other open source projects where noted.
-This code is licensed under:
-Creative Commons Attribution-ShareAlike 2.0 Generic (CC BY-SA 2.0)
-https://creativecommons.org/licenses/by-sa/2.0/
-Visit http://www.plumgeek.com for Ringo information.
-Visit http://www.arduino.cc to learn about the Arduino.
-
-*/
-
 #include "RingoHardware.h"
 
   const int MULT = 1;
